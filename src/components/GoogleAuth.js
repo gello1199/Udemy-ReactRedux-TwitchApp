eab61e341034c226e1a7ce2e756b0a976bef71be
Angelo Tsaganeas <a.tsaganeas@gmail.com>
2022-01-12 16:30:17 -0500

    Add gapi componentDidMount

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -1,6 +1,15 @@
 import React, { Component } from 'react';
 
 class GoogleAuth extends Component {
+    componentDidMount() {
+        window.gapi.load('client:auth2', () => {
+            window.gapi.client.init({
+                clientId: '431482947437-j8va0kggf5hsqtjpp50fkoc6lduoecf2.apps.googleusercontent.com',
+                scope: 'email'
+            })
+        })
+    }
+
     render() {
         return (
             <div>
