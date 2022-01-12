9455d16633927fe3e3753dc357742f19349b41d1
Angelo Tsaganeas <a.tsaganeas@gmail.com>
2022-01-12 13:44:04 -0500

    Add streams folder with Stream components

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,29 @@
 import React from 'react';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+
+const pageOne = () => {
+    return <div>PageOne
+    <Link to='/pagetwo'>Navigate to page Two</Link>
+    </div>
+}
+
+const pageTwo = () => {
+    return <div>
+    PageTwo
+    <button>Click Me!</button>
+    <Link to='/'>Navigate to page One</Link>
+    </div>
+}
 
 const App = () => {
     return (
         <div>
-            App
+            <BrowserRouter>
+                <div>
+                <Route path='/' exact component={pageOne} />
+                <Route path='/pagetwo' component={pageTwo} />
+                </div>
+            </BrowserRouter>
         </div>
     );
 }
