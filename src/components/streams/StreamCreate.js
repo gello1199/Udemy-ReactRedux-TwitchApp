aef02e47876db265d846d8b39f6d035ad0a312cd
Angelo Tsaganeas <a.tsaganeas@gmail.com>
2022-01-13 16:07:53 -0500

    Added different refactors of input in renderInput

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -2,13 +2,23 @@ import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form'
 
 class StreamCreate extends Component {
-    renderInput(formProps) {
-        return (
-        <input 
-            onChange={formProps.input.onChange} 
-            value={formProps.input.value}    
-        />
-        )
+    //  renderInput(formProps) {
+    //     return (
+    //     <input 
+    //     onChange={formProps.input.onChange}
+    //     value={formProps.input.value}
+    //     />
+    //     )
+    // }
+    //Below is a refactored version of above
+
+    // renderInput(formProps) {
+    //     return <input {...formProps.input} />
+    // }
+    // below is destructured version of above
+
+     renderInput({ input }) {
+        return <input {...input} />
     }
 
     render() {
