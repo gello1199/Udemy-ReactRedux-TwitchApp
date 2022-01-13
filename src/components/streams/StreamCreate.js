ec381177666d71007d59162c8d99d06377e8b6a2
Angelo Tsaganeas <a.tsaganeas@gmail.com>
2022-01-13 16:01:00 -0500

    Add input with onChange and value to renderInput

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -2,11 +2,21 @@ import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form'
 
 class StreamCreate extends Component {
+    renderInput(formProps) {
+        return (
+        <input 
+            onChange={formProps.input.onChange} 
+            value={formProps.input.value}    
+        />
+        )
+    }
+
     render() {
         return (
-            <div>
-                Stream Create
-            </div>
+            <form>
+                <Field name="title" component={this.renderInput} />
+                <Field name="description" component={this.renderInput} />
+            </form>
         );
     }
 }
