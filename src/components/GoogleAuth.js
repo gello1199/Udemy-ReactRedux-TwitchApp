f3a333797874465d23880822b6e718311480b2ec
Angelo Tsaganeas <a.tsaganeas@gmail.com>
2022-01-12 19:03:41 -0500

    Add getAuthInstance and local state to GoogleAuth component

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 
 class GoogleAuth extends Component {
+    state = { isSignedIn: null }
+
     componentDidMount() {
         window.gapi.load('client:auth2', () => {
             window.gapi.client.init({
                 clientId: '431482947437-j8va0kggf5hsqtjpp50fkoc6lduoecf2.apps.googleusercontent.com',
                 scope: 'email'
+            }).then(() => {
+                this.auth = window.gapi.auth2.getAuthInstance()
+                this.setState({isSignedIn: this.auth.isSignedIn.get()})
             })
         })
     }
