2a01fa12d4d56ff8e21d8fbcdda5f4d722464033
Angelo Tsaganeas <a.tsaganeas@gmail.com>
2022-01-14 16:11:07 -0500

    Add fetchStream action creator

--- src/actions/index.js
+++ src/actions/index.js
@@ -35,4 +35,10 @@ export const fetchStreams = () => async dispatch => {
     dispatch({ type: FETCH_STREAMS, payload: response.data })
 }
 
+export const fetchStream = (id) => async dispatch => {
+    const response = await streams.get(`/streams/${id}`)
+
+    dispatch({ type: FETCH_STREAM, payload: response.data })
+}
+
 
