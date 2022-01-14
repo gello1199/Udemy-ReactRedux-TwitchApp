7ea1d286e0ebe1cfb0ac93cb40152f0c4dfa6da3
Angelo Tsaganeas <a.tsaganeas@gmail.com>
2022-01-14 16:17:07 -0500

    Add deleteStream action creator

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,5 @@
 import streams from '../apis/streams'
+import StreamShow from '../components/streams/StreamShow'
 import { 
     SIGN_IN, 
     SIGN_OUT, 
@@ -45,4 +46,10 @@ export const editStream = (id, formValues) => async dispatch => {
     const response = await streams.put(`/streams/${id}`, formValues)
 
     dispatch({ type: EDIT_STREAM, payload: response.data })
+}
+
+export const deleteStream = (id) => async dispatch => {
+    await streams.delete(`/streams/${id}`)
+
+    dispatch({ type: DELETE_STREAM, payload: id })
 }
