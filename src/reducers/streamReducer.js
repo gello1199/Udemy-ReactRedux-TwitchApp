accdb6b3713b8cd64d3401c89721113197f8586d
Angelo Tsaganeas <a.tsaganeas@gmail.com>
2022-01-14 18:52:12 -0500

    Add FETCH_STREAM reducer

--- src/reducers/streamReducer.js
+++ src/reducers/streamReducer.js
@@ -5,3 +5,13 @@ import {
     EDIT_STREAM,
     DELETE_STREAM
 } from '../actions/types'
+
+export default (state = {}, action) => {
+    switch (action.type) {
+        case FETCH_STREAM:
+           return {...state, [action.payload.id]: action.payload}
+         
+        default:
+           return state;
+    }
+}
