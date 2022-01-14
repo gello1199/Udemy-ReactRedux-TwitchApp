4dd4f923a5047d3fb47bffd6c727a1048b96390a
Angelo Tsaganeas <a.tsaganeas@gmail.com>
2022-01-14 18:53:26 -0500

    Add CREATE_STREAM reducer

--- src/reducers/streamReducer.js
+++ src/reducers/streamReducer.js
@@ -10,7 +10,9 @@ export default (state = {}, action) => {
     switch (action.type) {
         case FETCH_STREAM:
            return {...state, [action.payload.id]: action.payload}
-         
+         case CREATE_STREAM:
+             return {...state, [action.payload.id]: action.payload}
+             
         default:
            return state;
     }
