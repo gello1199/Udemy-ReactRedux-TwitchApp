75f6c372ffbd58fe62ed46d2d642770f23e1a2d0
Angelo Tsaganeas <a.tsaganeas@gmail.com>
2022-01-16 23:01:22 -0500

    Start edit form

--- src/components/streams/StreamEdit.js
+++ src/components/streams/StreamEdit.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
-const StreamEdit = () => {
+const StreamEdit = (props) => {
+    console.log(props)
     return (
-        <div>
-           StreamEdit 
-        </div>
+        <form>
+          <input type="text" />
+        </form>
     );
 }
 
