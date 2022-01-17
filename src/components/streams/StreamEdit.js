81a033627142fb4898c3e174753264c9bb3d239c
Angelo Tsaganeas <a.tsaganeas@gmail.com>
2022-01-17 16:04:47 -0500

    Mount data from fetchStream to render title to edit page

--- src/components/streams/StreamEdit.js
+++ src/components/streams/StreamEdit.js
@@ -1,12 +1,24 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { fetchStream } from '../../actions'
 
-const StreamEdit = (props) => {
-    console.log(props)
-    return (
-        <form>
-          <input type="text" />
-        </form>
-    );
+class StreamEdit extends React.Component {
+    componentDidMount() {
+        this.props.fetchStream(this.props.match.params.id)
+    }
+
+    render() {
+        console.log(this.props)
+            if (!this.props.stream) {
+                return <div>Loading...</div>
+            }
+        return <div>{this.props.stream.title}</div> 
+    }
+  
 }
 
-export default StreamEdit;
+const mapStateToProps = (state, ownProps) => {
+    return { stream: state.streams[ownProps.match.params.id] }
+}
+
+export default connect(mapStateToProps, { fetchStream })(StreamEdit);
