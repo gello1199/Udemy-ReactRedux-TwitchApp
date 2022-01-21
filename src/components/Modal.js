b20cdf818ac18d5e2d85a722c3b9154e22603a8a
Angelo Tsaganeas <a.tsaganeas@gmail.com>
2022-01-21 15:58:02 -0500

    Create Modal portal component

---
+++ src/components/Modal.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import reactDOM from 'react-dom';
+
+const Modal = () => {
+    return (
+        <div>
+                
+        </div>
+    );
+}
+
+export default Modal;
