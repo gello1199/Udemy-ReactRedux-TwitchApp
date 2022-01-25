87b71f1a6ef3361bff253f56a720c2ff9052fd85
Angelo Tsaganeas <a.tsaganeas@gmail.com>
2022-01-24 22:06:02 -0500

    Add deleteStream to mapstatetoprops and added delete functionality

--- src/components/streams/StreamDelete.js
+++ src/components/streams/StreamDelete.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import Modal from '../Modal';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import history from '../../history';
-import { fetchStream } from '../../actions'
-import { Link } from 'react-router-dom'
+import { fetchStream, deleteStream } from '../../actions'
 
 class StreamDelete extends React.Component {
 
@@ -12,9 +12,10 @@ class StreamDelete extends React.Component {
     }
 
     renderActions() {
+        const id = this.props.match.params.id
          return (
             <React.Fragment>
-                <button className="ui button negative">Delete</button>
+                <button onClick={() => this.props.deleteStream(id)} className="ui button negative">Delete</button>
                 <Link to='/' className="ui button">Cancel</Link>
             </React.Fragment>
         )
@@ -43,4 +44,4 @@ const mapStateToProps = (state, ownProps ) => {
     return { stream: state.streams[ownProps.match.params.id] }
 }
 
-export default connect(mapStateToProps, { fetchStream })(StreamDelete);
+export default connect(mapStateToProps, { fetchStream, deleteStream })(StreamDelete);
